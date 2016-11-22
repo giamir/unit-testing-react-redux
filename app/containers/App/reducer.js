1bc4ff59d5f656b00faf481f4f0b89330df904c8
Giamir Buoncristiani <giamir@thoughtworks.com>
2016-11-22 00:21:34 +0000

    Add js linting to CI

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -9,15 +9,15 @@ const initialState = fromJS({
 
 function appReducer(state = initialState, action) {
   switch (action.type) {
-    case ADD_ITEM:
-      if (!state.get('textField')) return state;
-      return state
-        .set('textField', '')
-        .set('list', state.get('list').push(state.get('textField')));
-    case CHANGE_TEXT_FIELD:
-      return state.set('textField', action.value);
-    default:
-      return state;
+  case ADD_ITEM:
+    if (!state.get('textField')) return state;
+    return state
+      .set('textField', '')
+      .set('list', state.get('list').push(state.get('textField')));
+  case CHANGE_TEXT_FIELD:
+    return state.set('textField', action.value);
+  default:
+    return state;
   }
 }
 
